f66a903fb9d8dfd723f59a77dd07e9bc2c7b2318
inknsharps <nchen25@gmail.com>
2021-06-09 21:47:20 -0400

    add mongodb models

---
+++ api/models/CodeSnippet.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CodeSnippetSchema = new Schema({
+	snippetTitle: {
+		type: String,
+		default: "New Code Snippet!"
+	},
+	snippetContent: {
+		type: String,
+		default: "console.log('Hello World!')"
+	}
+});
+
+const CodeSnippet = mongoose.model("CodeSnippet". CodeSnippetSchema);
+
+module.exports = Workout;
---
+++ api/models/index.js
@@ -0,0 +1,1 @@
+module.exports = { CodeSnippet: require("./CodeSnippet") };
