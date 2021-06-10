0083b4b87ea103717f2d83a71e43705fe5d2e379
inknsharps <nchen25@gmail.com>
2021-06-09 23:10:22 -0400

    add cors

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(require("./routes"));
+app.use(cors);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/codepasta", { useNewUrlParser: true });
 
