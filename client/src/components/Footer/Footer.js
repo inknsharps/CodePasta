9fb894fc7c1cdf913fc2cede8d1f159dbe982c3b
inknsharps <nchen25@gmail.com>
2021-06-11 16:30:02 -0400

    update footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<footer className="text-base p-3 bg-gradient-to-l from-red-900 to-pink-800">
-			<h5>Made with React ⚛, Tailwind CSS 💨, 💉 & 😅 & 😢.</h5>
+			<h5>Made with <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React ⚛</a>, <a href="https://tailwindcss.com/" rel="noreferrer" target="_blank">Tailwind CSS 💨</a>, 💉 & 😅 & 😭.</h5>
 			<h6 className="text-sm">Neil Chen, 2021.</h6>
 		</footer>
 	)
